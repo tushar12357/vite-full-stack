7312470e8c8b5f57c3bc507fe1fd54594d175c45
tushar12357 <110051562+tushar12357@users.noreply.github.com>
2025-10-28 02:36:45 +0530

    Revert "listen"
    This reverts commit 98d5888741a709ca4672b781697768d0769aef51.

--- api/blogs.js
+++ api/blogs.js
@@ -81,9 +81,9 @@ app.delete("/api/blogs/:id", async (req, res) => {
   }
 });
 
-// app.listen(3000, () =>
-//   console.log("🚀 Server running on http://localhost:3000")
-// );
+app.listen(3000, () =>
+  console.log("🚀 Server running on http://localhost:3000")
+);
 
 // ✅ Export for Vercel
 export default serverless(app);
